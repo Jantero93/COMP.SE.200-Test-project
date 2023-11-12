470f0372d69c78555418f2a3658b34164849e59b
Jani Sillanpää <janzku1993@hotmail.com>
2023-11-12 06:05:31 +0200

    test pipeline with coverall

--- __tests__/sum.test.js
+++ __tests__/sum.test.js
@@ -5,10 +5,13 @@ describe('test set', () => {
   test('adds 1 + 2 to equal 3', () => {
     expect(add(1, 2)).toBe(3);
   });
-});
 
-describe('failing test set', () => {
-  test('always will fail', () => {
-    expect(isObject(1)).toBe(true);
+  test('testing test', () => {
+    expect(1).toBe(1);
+  });
+
+  test('isObject', () => {
+    const object = { test: 1, attribute: 'test' };
+    expect(isObject(object)).toBe(true);
   });
 });
